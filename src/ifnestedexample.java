1fc1da5a6b5cfdc6bba12c166f3f810a5099edde
risa1006 <risa@unida.ac.id>
2021-12-11 13:11:51 +0700

    percabangan bersarang

---
+++ src/ifnestedexample.java
@@ -0,0 +1,20 @@
+
+public class ifnestedexample {
+	public static void main (String[] args) {
+		int umur=17;
+		boolean hamil=false; 
+		if(umur>=18) {
+			if(hamil==false) {
+				System.out.println("Vaksin AstraZeneca");
+			} else {
+				System.out.println("Vaksin Sinovac");
+				System.out.println("Vaksin pfizer");
+				System.out.println("Vaksin Moderna");
+			}
+		} else {
+			System.out.println("Vaksin Sinovac");
+		}
+	}
+
+}
+
